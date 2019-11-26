0f244865148d2da50e0c167e12445e17125c8c93
Nisar Ahmed <nisar_ahmed77@hotmail.com>
2019-11-26 11:39:00 -0800

    First commit: adding all the files to the repository

---
+++ db/migrate/20191029232912_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :index
+      t.string :title
+      t.text :discription
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
